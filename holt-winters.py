d18c156b2a1e0a0ccbb68c5f38481a0523b0b139
Uzilea <168378949+Uzilea@users.noreply.github.com>
2024-05-31 16:26:01 +0200

    Update holt-winters.py

--- holt-winters.py
+++ holt-winters.py
@@ -13,6 +13,20 @@ import plotly.express as px
 dane = pd.read_excel('Do modelu.xlsx')
 
 st.header('Model Holt-Winters')
+st.subheader('Model Holta-Wintersa, nazywany również modeliem wygładzania wykładniczego, jest jednym z najpopularniejszych 
+modeli do prognozowania danych szeregów czasowych. Model ten jest szczególnie skuteczny przy prognozowaniu danych, 
+które wykazują zarówno sezonowość, jak i trend.')
+
+ll, rr = st.columns((2,2))
+
+
+kategorie=dane.columns
+kategorie=kategorie[2:]
+
+wybor_kat = ll.selectbox('Wybierz kategorię :', kategorie)
+
+st.subheader('Wybierz parametry modelu :')
+l, r = st.columns((2,2))
 
 
 
